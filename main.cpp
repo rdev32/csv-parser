0fde247acfdccdeb3e65e1301fb2b24b131ff4ee
Renato Monroy <78289548+rdev32@users.noreply.github.com>
2023-06-28 14:39:34 -0500

    feat: new method

--- main.cpp
+++ main.cpp
@@ -1,91 +1,69 @@
-#include <iostream>
-#include <fstream>
-#include <string>
 #include <vector>
-#include <map>
-#include <algorithm>
+#include <type_traits>
+#include <iomanip>
+#include <iostream>
+#include <typeindex>
+#include <typeinfo>
+#include <unordered_map>
+#include <charconv>
+#include <exception>
+#include <cstdlib>
 
-auto readLine(const std::string &line)
+int main()
 {
-    std::vector<std::string> lineContent{};
-    
-    std::string word{};
-    for (auto &letter : line) {
-        if (letter == ',') {
-            lineContent.push_back(word);
-            word = "";
-            continue;
+    auto readline =[](const std::string& line)
+    {
+        std::vector<std::string_view> cols;
+        char mode = ',';
+        char del =',';
+        char quote = '"';
+        size_t lastdel =0;
+        bool isquote = false;
+        size_t i = 0;
+        for(; i < line.size();++i)
+        {
+            //const char cur = ;
+            if(line[i]==mode)
+            {
+                if(mode == quote)
+                    ++lastdel;
+                if(mode == quote || (mode == del && !isquote))
+                {
+                cols.emplace_back(&line[lastdel],i-lastdel);
+                lastdel = i;
+                if(i + 1 < line.size())
+                    lastdel++;
+                }
+                else if(mode == del && isquote)
+                {
+                    isquote = false;
+                    lastdel++;
+                }
+            }
+            if(line[i]==quote)
+            {
+                switch(mode)
+                {
+                case ',':
+                    mode = quote;
+                    isquote = true;
+                    break;
+                case '"':
+                    mode = del;
+                    break;
+                }
+            }
         }
-        word += letter;
+        if(!isquote)
+          cols.emplace_back(&line[lastdel],i-lastdel);
+        return cols;
+    };
+    std::string line = "colname1,,col2,col3,,,\"col4\",col5,column6,\"columninquote\"";
+    auto vec = readline(line);
+    for(auto v:vec)
+    {
+        if(v.empty())
+          std::cout << "empty column\n";
+        else std::cout << v << '\n';
     }
-    lineContent.push_back(word);
-
-    return lineContent;
-}
-
-void removeSpaces(std::string &line)
-{
-    line.erase(
-        std::remove_if(
-            line.begin(), 
-            line.end(), 
-            [](unsigned char c) {
-                return std::isspace(c);
-            }), 
-        line.end());
-}
-
-auto headers(std::ifstream &file) 
-{ 
-    std::string attributes{};
-    std::getline(file, attributes);
-
-    removeSpaces(attributes);
-    return readLine(attributes);
-}
-
-auto contents(std::ifstream &file) 
-{
-    std::map<std::string, std::string> content;
-    auto headersDone { true };
-
-    while(file) 
-    {    
-        std::string line{};
-        std::getline(file, line);
-        removeSpaces(line);
-
-        if (headersDone) { 
-            std::make_pair(readLine(line), );
-            headersDone = false; 
-            continue; 
-        }
-
-        content.push_back(readLine(line));
-    }
-
-    return content;
-}
-
-int main(int argc, char* argv[])
-{
-    std::ifstream file{ std::string(argv[argc -1]) };
-    
-    if (!file) { 
-        std::cerr << "File not found.\n";
-        return 1;
-    }
-
-    auto head = headers(file);
-    auto content = contents(file);
-
-    for (auto &attribute : head) {
-        std::cout << attribute << " : ";
-        for (auto &value : content) {
-            
-        }
-        std::cout << std::endl;
-    }
-
-    return 0;
 }
